e9fbae1b519b3e9a37f7d1da4cea2f0116b52a08
niasua <nsuarez2410@gmail.com>
2025-07-23 17:49:38 -0300

    create the basis of the contact service methods

---
+++ PhoneBook/ContactService.cs
@@ -0,0 +1,64 @@
+﻿using PhoneBook.Data;
+using PhoneBook.Models;
+
+namespace PhoneBook;
+
+public class ContactService
+{
+    private readonly PhoneBookContext db = new();
+
+    public void AddContact(Contact contact)
+    {
+        db.Add(contact);
+        db.SaveChanges();
+    }
+
+    public List<Contact> GetAllContacts()
+    {
+        return db.Contacts
+            .OrderBy(c => c.Id)
+            .ToList();
+    }
+
+    public Contact? GetContactByName(string name)
+    {
+        return db.Contacts
+            .FirstOrDefault(c => c.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool ModifyContact(int id, string name, string email, string phoneNumber)
+    {
+        var contact = db.Contacts.FirstOrDefault(c => c.Id == id);
+
+        if (contact == null)
+        {
+            Console.WriteLine($"No contact found wih ID {id}");
+            return false;
+        }
+
+        contact.Name = name;
+        contact.Email = email;
+        contact.PhoneNumber = phoneNumber;
+
+        db.SaveChanges();
+
+        return true;
+    }
+
+    public bool DeleteContact(int id)
+    {
+        var contact = db.Contacts.FirstOrDefault(c => c.Id == id);
+
+        if (contact == null)
+        {
+            Console.WriteLine($"No contact found wih ID {id}");
+            return false;
+        }
+
+        db.Remove(contact);
+
+        db.SaveChanges();
+
+        return true;
+    }
+}
