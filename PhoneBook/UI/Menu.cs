27af5adc56ca47643d9c76c0c11d5ae6d75051f7
niasua <nsuarez2410@gmail.com>
2025-07-23 17:59:14 -0300

    adds menu structure

---
+++ PhoneBook/UI/Menu.cs
@@ -0,0 +1,69 @@
+﻿using Spectre.Console;
+using System.ComponentModel.DataAnnotations;
+
+namespace PhoneBook.UI;
+
+public static class Menu
+{
+    public static void ShowMenu()
+    {
+        var option = AnsiConsole.Prompt(
+            new SelectionPrompt<string>()
+            .Title("[blue]Phone Book Menu[/]")
+            .AddChoices(new[]
+            {
+                "Add Contact", "View Contacts", "Edit Contact","Delete Contact", "Exit"
+            }));
+
+        switch (option)
+        {
+            case "Add Contact":
+
+                AddContact();
+
+                break;
+
+            case "View Contacts":
+
+                ViewContacts();
+
+                break;
+
+            case "Edit Contact":
+
+                EditContact();
+
+                break;
+
+            case "Delete Contact":
+
+                DeleteContact();
+
+                break;
+
+            case "Exit":
+
+                break;
+        }
+    }
+
+    private static void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void EditContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void ViewContacts()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+}
