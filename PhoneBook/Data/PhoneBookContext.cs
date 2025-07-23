82b3ee8b70d3e302679dc00b5c88848e99285699
niasua <nsuarez2410@gmail.com>
2025-07-23 17:14:52 -0300

    adds phone book context

---
+++ PhoneBook/Data/PhoneBookContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+using PhoneBook.Models;
+
+namespace PhoneBook.Data;
+
+public class PhoneBookContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+    public string DbPath { get; set; }
+
+    public PhoneBookContext()
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        DbPath = System.IO.Path.Join(path, "phone_book.db");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite($"Data source={DbPath}");
+}
