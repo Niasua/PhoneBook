07d65900afb935d3991e75593d985ee330826613
niasua <nsuarez2410@gmail.com>
2025-07-24 18:30:54 -0300

    Initializes the seeder in the program.cs

--- PhoneBook/Program.cs
+++ PhoneBook/Program.cs
@@ -1,4 +1,10 @@
-﻿using PhoneBook.UI;
+﻿using PhoneBook.Data;
+using PhoneBook.UI;
+
+using (var context = new PhoneBookContext())
+{
+    DbInitializer.SeedCategories(context);
+}
 
 Menu menu = new();
 menu.Show();
